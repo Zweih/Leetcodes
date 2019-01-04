495734a7ad269d48c1b4b58597a3b9355e96f310
Fernando Nunez <fernandon299@gmail.com>
2019-01-04 16:10:20 -0500

    Formatting fix.

--- 74.js
+++ 74.js
@@ -1,4 +1,4 @@
-/*
+/**
   74. Search a 2D Matrix
   
   Execution time 52 ms, faster than 100% of JS submissions!
@@ -8,7 +8,7 @@
   Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
     - Integers in each row are sorted from left to right.
     - The first integer of each row is greater than the last integer of the previous row.
-*/
+ */
 
 /**
  * @param {number[][]} matrix
